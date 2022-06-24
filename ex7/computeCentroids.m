8813be4a17c5b4d3a3dbdeef96ca6baa63bcea64
Adam Tait <bin@adamta.it>
2022-06-23 17:24:37 -0700

    ex7: k-means algorithm working

--- ex7/computeCentroids.m
+++ ex7/computeCentroids.m
@@ -1,7 +1,7 @@
 function centroids = computeCentroids(X, idx, K)
-%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%COMPUTECENTROIDS returns the new centroids by computing the means of the
 %data points assigned to each centroid.
-%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by
 %   computing the means of the data points assigned to each centroid. It is
 %   given a dataset X where each row is a single data point, a vector
 %   idx of centroid assignments (i.e. each entry in range [1..K]) for each
@@ -26,12 +26,28 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+%zs = zeros(1, size(X,2) );
+%xsbyc = [zs; zs; zs];
+%xsbyc = zeros(1, size(X,2), K);
 
+%for j = 1:size(idx,1)
+%  id = idx(j);
+%  xsbyc(id) = [ xsbyc(id) ; X(j,:) ];
+%endfor
 
-
-
-
-
+for i = 1:K
+  %xs = xsbyc(i);
+  s = zeros(1, n);
+  cn = 0;
+  for j = 1:size(idx,1)
+    if ( idx(j) == i )
+      s = s + X(j,:);
+      cn = cn + 1;
+    end
+  endfor
+  %centroids(i) = sum(xs) * ( 1 / size(xs, 1) );
+  centroids(i,:) = s .* ( 1 / cn );
+endfor
 
 % =============================================================
 
